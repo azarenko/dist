d9e509180d73147007eef2b2b7dbf0a3c9510ccd
sergey__azarenko <sergey.azarenko@valtech.com>
2019-08-30 14:55:30 +0300

    first release

---
+++ dist/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace dist
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Dictionary<int, bool> hashes = new Dictionary<int, bool>();
+
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    int hash = line.GetHashCode();
+
+                    if (!hashes.ContainsKey(hash))
+                    {
+                        hashes.Add(hash, true);
+                        Console.WriteLine(line);                        
+                    }
+                }
+            }
+        }
+    }
+}
